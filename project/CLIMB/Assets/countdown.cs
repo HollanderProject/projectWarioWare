218e093d457893dcaf1e019bc3b53972c3a338a4
Cameron Bernard <bernardc@knights.ucf.edu>
2021-02-05 20:05:51 -0500

    Added first draft of CLIMB microgame

---
+++ project/CLIMB/Assets/countdown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class countdown : MonoBehaviour
+{
+    public TMP_Text timerText;
+    public GameObject failText;
+    public GameObject winText;
+    public GameObject climber;
+    public float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(time > 0 && !winText.activeSelf)
+        {
+            // Counts down time and displays it
+            time -= Time.deltaTime;
+            timerText.text = ((int)time).ToString();
+        }
+        else if(time <= 0 && !winText.activeSelf)
+        {
+            // Creates a failure state if time hits 0
+            failText.SetActive(true);
+            climber.GetComponent<Rigidbody2D>().gravityScale = 0;
+            climber.SetActive(false);
+        }
+    }
+    
+}
