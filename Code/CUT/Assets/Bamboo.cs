4527d9cd2b932983483aed9413df80bf3649fe1b
Niadu <blurreddawg@gmail.com>
2021-02-21 04:36:45 -0500

    Added Climb and Cut microgames to the Code folder

---
+++ Code/CUT/Assets/Bamboo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bamboo : MonoBehaviour
+{
+    public GameObject winText;
+    public GameObject failText;
+    public GameObject bamboo;
+    void start()
+    {
+        winText.SetActive(false);
+        failText.SetActive(false);
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Scissors")
+        {
+            winText.SetActive(true);
+            bamboo.GetComponent<Rigidbody2D>().gravityScale = 0;
+            bamboo.SetActive(false);
+            Debug.Log("Collision detected.");
+        }
+    }
+}
---
+++ Code/CUT/Assets/Countdown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Countdown : MonoBehaviour
+{
+    public TMP_Text timerText;
+    public GameObject failText;
+    public GameObject winText;
+    public GameObject scissors;
+    public GameObject Bamboo;
+    public float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0 && !winText.activeSelf)
+        {
+            // Counts down time and displays it
+            time -= Time.deltaTime;
+            timerText.text = ((int)time).ToString();
+        }
+        else if (time <= 0 && !winText.activeSelf)
+        {
+            // Creates a failure state if time hits 0
+            failText.SetActive(true);
+            Bamboo.SetActive(false);
+        }
+    }
+
+}
