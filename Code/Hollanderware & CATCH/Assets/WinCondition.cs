4fa7853b80f0b5ef7db834178e28996cda789b73
Noah <noahpparentreason@gmail.com>
2021-02-21 22:17:41 -0500

    Added menu, settings, and CATCH microgame prototype

---
+++ Code/Hollanderware & CATCH/Assets/WinCondition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinCondition : MonoBehaviour
+{
+    Text winText;
+
+    void Start()
+    {
+        winText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ScoreScript.scoreValue >= ScoreScript.scoreGoal)
+        {
+            winText.text = "You Win!";
+        }
+        else if (FailScript.failScoreValue >= FailScript.failGoal)
+        {
+            winText.text = "You Lose";
+        }
+    }
+}
