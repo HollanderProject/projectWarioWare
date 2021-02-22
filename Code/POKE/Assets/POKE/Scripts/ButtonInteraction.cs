3d686ad35729d0cd1beb63c30027fe8e84482b0c
Noah Law <noahlaw@knights.ucf.edu>
2021-02-21 20:28:33 -0500

    Added Poke microgames to the Code folder
    Added the prototype version of the Poke microgame to the Code folder

---
+++ Code/POKE/Assets/POKE/Scripts/ButtonInteraction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonInteraction : MonoBehaviour
+{
+
+    int noseClicks = 0;
+
+    void Update ()
+    {   
+        
+    }
+
+    private void OnMouseDown()
+    {
+        /*
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+
+        }
+        */
+        noseClicks++;
+        Debug.Log("Nose click number: " + noseClicks);
+        
+        if (noseClicks >= 3)
+        {
+            Debug.Log("Win condition has been met");
+            ChangeScene("winScene");
+        }
+    }
+    
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+}
