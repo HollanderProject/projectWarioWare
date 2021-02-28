f4e00c9ac361d406ae86327d4fbed5f97e5f0206
Niadu <blurreddawg@gmail.com>
2021-02-28 05:40:23 -0500

    Improvements to CUT

---
+++ Code/CUT/Assets/Tests/PlayMode/BladeCutActive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BladeCutActive
+    {
+        [UnityTest]
+        public IEnumerator BladeCutActives()
+        {
+            // Use the Assert class to test conditions.
+            var gameObject = new GameObject();
+            var blade = gameObject.AddComponent<Scissors>();
+
+            blade.isCutting = true;
+
+            // Use yield to skip a frame.
+
+            Assert.IsTrue(gameObject.activeSelf);
+            yield return (null);
+        }
+    }
+}
