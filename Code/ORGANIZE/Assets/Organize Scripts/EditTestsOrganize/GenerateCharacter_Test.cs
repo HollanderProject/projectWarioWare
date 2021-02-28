201057f51d5b81356132cf92484e7f11a8e89abf
Cameron Bernard <bernardc@knights.ucf.edu>
2021-02-28 13:03:39 -0500

    Finished ORGANIZE Prototype
    Also added automated tests for both CLIMB and ORGANIZE

---
+++ Code/ORGANIZE/Assets/Organize Scripts/EditTestsOrganize/GenerateCharacter_Test.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class GenerateCharacter_Test : MonoBehaviour
+{
+    public Sprite Male;
+    public Sprite Female;
+
+    [Test]
+    public void NoTimeLosing()
+    {
+        GameObject character = new GameObject("Character_1");
+        character.AddComponent<SpriteRenderer>();
+        var rand = new System.Random();
+        int Gender = rand.Next() % 2;
+
+        if(Gender == 1)
+        {
+            character.GetComponent<SpriteRenderer>().sprite = Male;
+        }
+        else
+        {
+            character.GetComponent<SpriteRenderer>().sprite = Female;
+        }
+
+        if(Gender == 1)
+        {
+            Assert.IsTrue(character.GetComponent<SpriteRenderer>().sprite == Male);
+        }
+        else
+        {
+            Assert.IsTrue(character.GetComponent<SpriteRenderer>().sprite == Female);
+        }
+    }
+
+}
