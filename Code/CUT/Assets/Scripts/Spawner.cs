751f0b523fbdaf1ad28551ddef9db7d3530d79f7
Niadu <blurreddawg@gmail.com>
2021-02-28 06:20:01 -0500

    Improvements to CUT

---
+++ Code/CUT/Assets/Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject bambooPrefab;
+    public Transform[] spawnPoints;
+    public GameObject winText;
+    public GameObject failText;
+
+    public float minDelay = .1f;
+    public float maxDelay = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnBamboo());
+    }
+
+    IEnumerator SpawnBamboo()
+    {
+        // While the game is not won or loss, loop through
+        while (!winText.activeSelf && !failText.activeSelf)
+        {
+            // Randomly pick a delay time to spawn a new bamboo
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(delay);
+
+            // Randomly pick a bamboo spawn point
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+            // Spawn the bamboo
+            GameObject spawnBamboo = Instantiate(bambooPrefab, spawnPoint.position, spawnPoint.rotation);
+            // Despawn after 2 seconds, to avoid clutter
+            Destroy(spawnBamboo, 2f);
+        }
+    }
+}
