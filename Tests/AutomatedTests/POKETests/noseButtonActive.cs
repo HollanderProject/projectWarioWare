622b2d1ead90e58fa18fc6efa925ab791643f8d8
noah-segfault <noahlaw@knights.ucf.edu>
2021-02-28 23:46:19 -0500

    Added test cases for POKE to Tests folder

---
+++ Tests/AutomatedTests/POKETests/noseButtonActive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    // Check if the button is acting as intended (default values are set and you actually go to the lose screen when the time comes)
+    public class loseScreenActive
+    {
+        [UnityTest]
+        public IEnumerator loseScreenActiveWithEnumeratorPasses()
+        {
+            yield return null;
+            Assert.AreEqual(ButtonInteraction.noseClicks, 0);
+            Assert.AreEqual(ButtonInteraction.clicksRequired, 10);
+            
+            // TODO: ADD THE LOSE CONDITION TEST
+        }
+    }
+}
