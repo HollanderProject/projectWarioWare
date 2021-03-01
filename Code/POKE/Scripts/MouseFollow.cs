d571de5295a2bb5b747b19b7f00a775cd8521e63
noah-segfault <noahlaw@knights.ucf.edu>
2021-02-28 23:36:19 -0500

    Updated Poke's visuals and added unit test cases

--- Code/POKE/Scripts/MouseFollow.cs
+++ Code/POKE/Scripts/MouseFollow.cs
@@ -2,16 +2,19 @@
 
 public class MouseFollow : MonoBehaviour
 {
+   // Create the variables for the mouse movement
    Vector3 mousePosition;
    public float moveSpeed = 1f;
    Rigidbody2D rb;
    Vector2 position = new Vector2(0f,0f);
-
+    
+   // Get the rigidbody of the mouse finger
    private void Start()
    {
        rb = GetComponent<Rigidbody2D>();
    }
-
+   
+   // Make the mouse finger follow the position of the player's mouse
    private void Update()
    {
        mousePosition = Input.mousePosition;
@@ -19,6 +22,7 @@ public class MouseFollow : MonoBehaviour
        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
    }
 
+   // Update the position of the finger as the game goes on 
    private void FixedUpdate()
    {
        rb.MovePosition(position);
