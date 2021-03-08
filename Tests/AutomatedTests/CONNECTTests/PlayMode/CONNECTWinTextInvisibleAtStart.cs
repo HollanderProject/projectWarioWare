7ab2cdebc91157750ae774a4147beb74e2727374
Noah <noahpparentreason@gmail.com>
2021-03-08 00:07:30 -0500

    Added automated tests for CONNECT and CATCH to the folder with other automated tests

---
+++ Tests/AutomatedTests/CONNECTTests/PlayMode/CONNECTWinTextInvisibleAtStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CONNECTWinTextInvisibleAtStart
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator CONNECTWinTextInvisibleAtStartWithEnumeratorPasses()
+        {
+            yield return null;
+            Assert.AreEqual(WinText.winText.color.a, new Color(1f, 1f, 1f, 0f));
+        }
+    }
+}
