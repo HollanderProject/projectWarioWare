261381b609056992f67f5a02834eb21ff76286f7
rinriukato <77295985+rinriukato@users.noreply.github.com>
2021-03-14 20:49:28 -0400

    updates to BROWSE and SHOOT, test as well

---
+++ Code/Browse/Assets/Tests/CountdownTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CountdownTest
+    {
+        GameObject failText;
+        GameObject winText;
+
+        [Test]
+        public void NoTimeLosing()
+        {
+            winText = new GameObject();
+            failText = new GameObject();
+
+            winText.SetActive(false);
+            failText.SetActive(false);
+
+            int time = 0;
+            Debug.Log(winText.activeSelf);
+            if(time <= 0 && !winText.activeSelf)
+            {
+                failText.SetActive(true);
+            }
+
+            Assert.IsTrue(failText.activeSelf);
+        }
+    }
+}
