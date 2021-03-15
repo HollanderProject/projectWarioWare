1d7d523b6fa762949517352f1d6803affdfc6d46
Noah Lang <noahpparentreason@gmail.com>
2021-03-14 23:39:18 -0400

    Level select added w/ auto tests

---
+++ Code/Hollanderware/Assets/Game Selection/EditMode/CUT_Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CUT_Button
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CUT_ButtonSimplePasses()
+        {
+            string scene = System.IO.Path.GetFileNameWithoutExtension(UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(PlayMicrogame.cut));
+
+            Assert.AreEqual("CUT Scene", scene);
+        }
+    }
+}
---
+++ Code/Hollanderware/Assets/Game Selection/EditMode/NUDGE_Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NUDGE_Button
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NUDGE_ButtonSimplePasses()
+        {
+            string scene = System.IO.Path.GetFileNameWithoutExtension(UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(PlayMicrogame.nudge));
+
+            Assert.AreEqual("NUDGE Scene", scene);
+        }
+    }
+}
