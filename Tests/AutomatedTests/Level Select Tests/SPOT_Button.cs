bd906e603302e5333b82a00d6698b6e02768034c
Noah Lang <noahpparentreason@gmail.com>
2021-03-15 00:15:47 -0400

    Added level select automated tests

---
+++ Tests/AutomatedTests/Level Select Tests/SPOT_Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class SPOT_Button
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void SPOT_ButtonSimplePasses()
+        {
+            string scene = System.IO.Path.GetFileNameWithoutExtension(UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(PlayMicrogame.spot));
+
+            Assert.AreEqual("SPOT Scene (Placeholder)", scene);
+        }
+    }
+}
