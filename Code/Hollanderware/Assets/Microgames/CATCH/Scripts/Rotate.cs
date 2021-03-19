9d50c83cfe4fe32b078c147f5f232bff155692b1
Noah Lang <noahpparentreason@gmail.com>
2021-03-18 23:09:47 -0400

    Updated CATCH graphics

--- Code/Hollanderware/Assets/Microgames/CATCH/Scripts/Rotate.cs
+++ Code/Hollanderware/Assets/Microgames/CATCH/Scripts/Rotate.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    public float RotationSpeed;
+    public float RotationSpeedMax;
+    public float RotationSpeedMin;
+    private float RotationSpeed;
     private float rotZ;
     public float smoothness = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        RotationSpeed = Random.Range(RotationSpeedMin, RotationSpeedMax);
     }
 
     // Update is called once per frame
