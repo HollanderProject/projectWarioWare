94c0c9f8579854d9c60a0f31a875b1ddbc46ff5b
noah-segfault <noahlaw@knights.ucf.edu>
2021-03-26 12:21:30 -0400

    Updated SPOTs UI and organized folders

---
+++ Code/SPOT/SPOT/Hollanderware/Assets/Scenes/SPOT/Scripts/checkPlaySound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkPlaySound : MonoBehaviour
+{
+    void playSound()
+    {
+        AudioSource sound = GetComponent<AudioSource>();
+        sound.Play();
+        Debug.Log("Sound shall play");
+    }
+
+}
