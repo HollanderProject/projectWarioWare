2bac7a02ef27b3224712529447af19fe5015ec11
noah-segfault <noahlaw@knights.ucf.edu>
2021-03-27 18:35:26 -0400

    Added POKE to the mircogames collection

---
+++ Code/Hollanderware/Assets/Microgames/POKE/Scripts/CountdownTimer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class CountdownTimer : MonoBehaviour
+{
+    // Create variables for what the timer should start at and the currenTime, which will countdown as the game goes on
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    // Create a textfield to be changed from the options
+    [SerializeField] Text countdownText;
+    
+    // Set the current time to 10 when the game starts
+    void Start()
+    {
+        currentTime = startingTime;
+       
+    }
+
+    // Countdown as the game goes on
+    void Update()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0"); 
+
+        // Once the game reaches 3 seconds turn the text to red to indicate urgency to the player
+        if (currentTime <= 3)
+        {
+            countdownText.color = Color.red;
+        }
+
+        // If the countdown reaches zero take the player to the losing screen
+        /*
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            if (currentScene == "Spot")
+            {
+                ChangeScene("spotLoseScreen");
+            }
+            else
+            {
+               ChangeScene("loseScene");
+            }
+            
+            
+        }
+        */
+    }
+
+    // Load up and display the losing screen
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Code/Hollanderware/Assets/Microgames/POKE/Scripts/MouseFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+   // Create the variables for the mouse movement
+   Vector3 mousePosition;
+   public float moveSpeed = 1f;
+   Rigidbody2D rb;
+   Vector2 position = new Vector2(0f,0f);
+    
+   // Get the rigidbody of the mouse finger
+   private void Start()
+   {
+       rb = GetComponent<Rigidbody2D>();
+   }
+   
+   // Make the mouse finger follow the position of the player's mouse
+   private void Update()
+   {
+       mousePosition = Input.mousePosition;
+       mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+       position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+   }
+
+   // Update the position of the finger as the game goes on 
+   private void FixedUpdate()
+   {
+       rb.MovePosition(position);
+   }
+
+}
