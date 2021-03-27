a4894d9518eb549f221cddf20a5c20f3b8caa2d3
noah-segfault <noahlaw@knights.ucf.edu>
2021-03-27 17:31:00 -0400

    Updated SPOT in the microgames folder

---
+++ Code/Hollanderware/Assets/Microgames/SPOT/SPOT/Scripts/SpotGraphicTimer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotGraphicTimer : MonoBehaviour
+{
+    public SpriteRenderer currentFrame;
+    public Sprite[] batteryFrames; // NOTE: Set array animation in reverse.
+
+    public float startingTime = 5f;
+    public float currentTime = 0f;
+
+    private bool keepCounting = true;
+
+    GameManagerSpot _gameManager;
+
+    void Start()
+    {
+        currentFrame = gameObject.GetComponent<SpriteRenderer>();
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManagerSpot>();
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        
+        if (_gameManager.playerWin)
+        {
+            keepCounting = false;
+        }
+        
+
+        if (keepCounting)
+        {
+            currentTime -= 1 * Time.deltaTime;
+            changeBatteryFrame(currentTime);
+        }
+
+        if (currentTime <= 0)
+        {
+            _gameManager.setPlayerLoseTrue();
+            keepCounting = false;
+        }
+    }
+
+    void changeBatteryFrame(float index)
+    {
+        int roundedIndex = (int)Mathf.Ceil(index);
+        currentFrame.sprite = batteryFrames[roundedIndex];
+    }
+
+
+}
